de9edcb07e1e35c7161c4387800dd018de627731
Mark Liebrand <developer@liebrand.eu>
2016-10-24 16:11:22 +0200

    Added start / stop scripts

---
+++ UdpPipe/udppipe<tail>.sh
@@ -0,0 +1,6 @@
+# sample start/stop script for head (commands are start/stop/restart)
+# copy to head host and rename it to udppipe.sh
+# pid file is by default /tmp/udppipe.pid
+# log file is by default /tmp/udppipe.log
+export PYTHONPATH=.
+python eu/liebrand/udppipe/__init__.py $1 -T
