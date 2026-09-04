9f31fc0989ece6d35b1e4acade24ee72d703cdd0
Mark Liebrand <developer@liebrand.eu>
2026-09-04 16:41:14 +0200

    Minor corrections

--- eu/liebrand/udppipe/Pipe.py
+++ eu/liebrand/udppipe/Pipe.py
@@ -943,28 +943,29 @@ class Tail(PipeBase):
                                 pass
                         if r==self.controlPipe[0]:
                             os.read(self.controlPipe[0],1)
-                            data=self.responseQ.get()
-                            dataBuffer=BytesIO()
-                            sockWt.writeString(PipeBase.FIELD_OP, PipeBase.VALUE_UDP, dataBuffer)
-                            sockWt.writeString(PipeBase.FIELD_HOST, data[PipeBase.FIELD_HOST], dataBuffer)
-                            sockWt.writeLong(PipeBase.FIELD_PORT, data[PipeBase.FIELD_PORT], dataBuffer)
-                            sockWt.writeLong(PipeBase.FIELD_SRVPORT, data[PipeBase.FIELD_SRVPORT], dataBuffer)
-                            sockWt.writeBinary(PipeBase.FIELD_UDPDATA, data[PipeBase.FIELD_UDPDATA], dataBuffer)
-                            dta=dataBuffer.getvalue()
-                            ctlBuffer=BytesIO()
-                            sockWt.writeLongDirect(len(dta), ctlBuffer)
-                            sockWt.writeBinaryDirect(dta, ctlBuffer)
-                            dta=ctlBuffer.getvalue()
-                            bytesSnd=0
-                            while bytesSnd<len(dta):
-                                bytesSnd=bytesSnd+servSocket.send(dta[bytesSnd:])
-                            dataBuffer.close()
-                            ctlBuffer.close()
-                            self.responseQ.task_done()
-                            self.TCPBytesOut+=bytesSnd
-                            self.packetsOut+=1
-                            self.log.debug("[Tail] Forwarded response packet of %d bytes for listening port %d from client %s:%d to >Head<" % \
-                                            (len(data[PipeBase.FIELD_UDPDATA]), data[PipeBase.FIELD_SRVPORT], data[PipeBase.FIELD_HOST], data[PipeBase.FIELD_PORT]  ) )
+                            if self.responseQ.not_empty:
+                                data=self.responseQ.get()
+                                dataBuffer=BytesIO()
+                                sockWt.writeString(PipeBase.FIELD_OP, PipeBase.VALUE_UDP, dataBuffer)
+                                sockWt.writeString(PipeBase.FIELD_HOST, data[PipeBase.FIELD_HOST], dataBuffer)
+                                sockWt.writeLong(PipeBase.FIELD_PORT, data[PipeBase.FIELD_PORT], dataBuffer)
+                                sockWt.writeLong(PipeBase.FIELD_SRVPORT, data[PipeBase.FIELD_SRVPORT], dataBuffer)
+                                sockWt.writeBinary(PipeBase.FIELD_UDPDATA, data[PipeBase.FIELD_UDPDATA], dataBuffer)
+                                dta=dataBuffer.getvalue()
+                                ctlBuffer=BytesIO()
+                                sockWt.writeLongDirect(len(dta), ctlBuffer)
+                                sockWt.writeBinaryDirect(dta, ctlBuffer)
+                                dta=ctlBuffer.getvalue()
+                                bytesSnd=0
+                                while bytesSnd<len(dta):
+                                    bytesSnd=bytesSnd+servSocket.send(dta[bytesSnd:])
+                                dataBuffer.close()
+                                ctlBuffer.close()
+                                self.responseQ.task_done()
+                                self.TCPBytesOut+=bytesSnd
+                                self.packetsOut+=1
+                                self.log.debug("[Tail] Forwarded response packet of %d bytes for listening port %d from client %s:%d to >Head<" % \
+                                                (len(data[PipeBase.FIELD_UDPDATA]), data[PipeBase.FIELD_SRVPORT], data[PipeBase.FIELD_HOST], data[PipeBase.FIELD_PORT]  ) )
             except socket.error as e:
                 if e.errno == errno.EINTR and self._terminate:
                     pass
