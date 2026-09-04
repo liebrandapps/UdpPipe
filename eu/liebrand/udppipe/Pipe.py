4b824423e754fb411e5bf1456b2c00f2f642c692
Mark Liebrand <developer@liebrand.eu>
2026-09-04 13:41:29 +0200

    Minor corrections

--- eu/liebrand/udppipe/Pipe.py
+++ eu/liebrand/udppipe/Pipe.py
@@ -421,6 +421,7 @@ class Head(PipeBase):
                             while len(dta)<5:
                                 dta+=r.recv(5-len(dta))
                             sockRd= eu.liebrand.udppipe.Utility.SockRead()
+                            eu.liebrand.udppipe.Utility.dump(dta)
                             buf=BytesIO(dta)
                             _,_,length=sockRd.read(buf)
                             data=[]
